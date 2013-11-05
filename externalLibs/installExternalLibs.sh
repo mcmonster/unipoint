690b85243e45ffd6bbffff5424373fa903c10d95
R. Matt McCann <mccann.matt@gmail.com>
2013-11-05 13:35:33 -0500

    Added temporary external dependencies folder with installation script

---
+++ externalLibs/installExternalLibs.sh
@@ -0,0 +1,6 @@
+mvn install:install-file -Dfile=openmap-5.0.1.jar  \
+                         -DgroupId=com.bbn \
+                         -DartifactId=openmap \
+                         -Dversion=5.0.1 \
+                         -Dpackaging=jar
+
